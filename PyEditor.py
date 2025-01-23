4b25958a1d0af0be852d32e9fcc86afdbe5d5a3a
GregoryVDev <arexo@hotmail.fr>
2025-01-23 20:04:29 +0100

    Added code to write on the editor window

--- PyEditor.py
+++ PyEditor.py
@@ -1,6 +1,9 @@
 # Créatop, de la fenêtre principale et de la zone de texte
 from tkinter import *
 
+from PIL.ImageOps import expand
+
+
 class PyEditor:
     def __init__(self, master):
         self.master = master
@@ -11,6 +14,8 @@ class PyEditor:
 
     def create_textarea(self):
         self.textarea = Text(self.master, font=("ubuntu", 18))
+        # Mettre un pack pour activer la zone de text (expand permet d'étaler le texte sur toute la fenêtre et fill BOTH pour dire verticalement et horizontalement)
+        self.textarea.pack(side=LEFT, expand = True, fill= BOTH)
 
 
 # Condition spéciale utilisée pour contrôler l'exécution d'un script
@@ -18,6 +23,8 @@ if __name__ == "__main__":
     master = Tk()
     editor = PyEditor(master)
     editor.create_window()
+    # Permet d'écrire sur la fenêtre
+    editor.create_textarea()
 
 
     master.mainloop()
